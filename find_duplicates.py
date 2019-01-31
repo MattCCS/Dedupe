e7f6bc55b951f66929609f27584383cfd9de344d
Matt Cotton <mcotton@racap.com>
2019-01-31 13:24:10 -0500

    - Port to Python3 - Use scandir - Use xxhash, add requirements.txt

--- find_duplicates.py
+++ find_duplicates.py
@@ -9,8 +9,9 @@ This script identifies all duplicate copies of files across the system.
 import collections
 import os
 import pathlib
-import subprocess
 import sys
+
+import xxhash
 
 
 HOME = pathlib.Path("~").expanduser() / "Documents"
@@ -18,38 +19,83 @@ assert HOME.exists()
 
 DUPLICATES = 0
 TOTAL = 0
+TOTAL_BYTES = 0
+DUPLICATE_BYTES = 0
+
+SIZE_MIN = 1 * 1024 * 1024
 
 
-def calc_md5(filepath):
-    return subprocess.check_output(["md5", filepath]).split(b' ')[-1].strip()
+def human_bytes(n):
+    """Return the given bytes as a human-friendly string"""
+
+    step = 1024
+    abbrevs = ['KB', 'MB', 'GB', 'TB']
+
+    if n < step:
+        return f"{n}B"
+
+    for abbrev in abbrevs:
+        n /= step
+        if n < step:
+            break
+
+    return f"{n:.2f}{abbrev}"
 
 
-def calc_size(filepath):
-    return int(subprocess.check_output(["ls", "-l", filepath]).split()[4])
+def fast_hash(path, buffer=8192):
+    """
+    Hashes the contents of the given filepath in chunks.
+    Returns a hex digest (0-9a-f) of the SHA256 hash.
+    Performance on a Macbook Pro is about 384 MB/s.
+    """
+
+    hash_obj = xxhash.xxh64()
+
+    with open(path, "rb") as infile:
+        data = infile.read(buffer)
+        while data:
+            hash_obj.update(data)
+            data = infile.read(buffer)
+
+    return hash_obj.hexdigest()
 
 
-def yield_filepaths(path):
+def scantree(path):
+    """Recursively yield DirEntry objects for given directory."""
+    if str(path).startswith('.'):
+        return
+
+    for entry in os.scandir(path):
+        if entry.name.startswith('.'):
+            continue
+
+        if entry.is_symlink():
+            return
+        elif entry.is_dir():
+            yield from scantree(entry.path)
+        else:
+            yield entry
+
+
+def yield_entries(path):
     global TOTAL
 
-    gen = os.walk(path)
-    for (path, directories, filenames) in gen:
-        for directory in directories:
-            if directory.startswith('.'):
-                directories.remove(directory)
+    gen = scantree(path)
+    for entry in gen:
 
-        for filename in [f for f in filenames if not f.startswith('.')]:
-            filepath = pathlib.Path(path) / filename
+        filename = entry.name
+        if filename.startswith('.'):
+            continue
 
-            if filepath.is_symlink():
-                continue
-            elif str(filepath).endswith("__init__.py"):
-                continue
-            elif str(filepath).endswith(".pyc"):
-                continue
+        # elif str(filepath).endswith("__init__.py"):
+        #     continue
+        # elif str(filepath).endswith(".pyc"):
+        #     continue
 
-            TOTAL += 1
-            sys.stdout.write("{} files found\r".format(TOTAL))
-            yield filepath
+        TOTAL += 1
+        if not TOTAL % 100:
+            sys.stdout.write(f"{TOTAL} files found\r")
+        yield entry
 
 
 def group_matches_by(filepaths, func):
@@ -68,22 +114,33 @@ def group_matches_by(filepaths, func):
 def main():
     global DUPLICATES
     global TOTAL
+    global TOTAL_BYTES
+    global DUPLICATE_BYTES
 
-    filepaths = yield_filepaths(HOME)
+    all_entries = yield_entries(HOME)
 
-    sizes = group_matches_by(filepaths, calc_size)
+    sizes = group_matches_by(all_entries, lambda e: e.stat().st_size)
     del sizes[0]
 
     print("\n\n")
-    for (size, filepaths) in list(sizes.items()):
-        hashes = group_matches_by(filepaths, calc_md5)
-        if hashes:
-            print("{}".format(size))
-            for (md5, filepaths) in list(hashes.items()):
-                print("  {}".format(md5))
-                for filepath in filepaths:
-                    print("    {}".format(filepath))
+    for (size, entries_by_size) in sorted(sizes.items()):
+        if size < SIZE_MIN:
+            continue
+
+        entries_by_hash = group_matches_by(entries_by_size, fast_hash)
+        if entries_by_hash:
+            print(f"{size} ({human_bytes(size)})")
+            for (hash, entries) in entries_by_hash.items():
+                TOTAL_BYTES += size * len(entries)
+                DUPLICATE_BYTES += size * (len(entries) - 1)
+                print(f"  {hash}")
+                for entry in entries:
+                    print(f"    {entry.path}")
                 print()
+
+    print(f"TOTAL FILES VISITED: {TOTAL:,}")
+    print(f"TOTAL BYTES HASHED: {TOTAL_BYTES} ({human_bytes(TOTAL_BYTES)})")
+    print(f"TOTAL DUPLICATE BYTES: {DUPLICATE_BYTES} ({human_bytes(DUPLICATE_BYTES)})")
 
 
 if __name__ == '__main__':
